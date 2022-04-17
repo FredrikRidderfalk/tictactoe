a550986560567ad49bafcacb0991a625bcba3236
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-04-17 15:19:06 -0400

    feat: add winning combinations

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  //HORIZONTAL
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  //VERTICAL
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  //DIAGONAL
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const board = document.querySelector(".board");
 const cells = document.querySelectorAll(".cell");
 
-let circleTurn = false;
+let circleTurn;
 
-cells.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
+startGame();
+
+function startGame() {
+  circleTurn = false;
+  cells.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
 
 // handleClick
 function handleClick(e) {
